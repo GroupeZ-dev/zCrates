3a25d217b414a581db90aed8d315d8347b5dfe43
Traqueur_ <traqueur.dev@gmail.com>
2025-11-14 13:47:00 +0100

    feat: add implementation of animation

---
+++ src/main/java/fr/traqueur/crates/animations/ZAnimation.java
@@ -0,0 +1,17 @@
+package fr.traqueur.crates.animations;
+
+import fr.traqueur.crates.api.models.animations.Animation;
+import fr.traqueur.crates.api.models.animations.AnimationContext;
+import fr.traqueur.crates.api.models.animations.AnimationPhase;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public record ZAnimation(String id, String sourceFile, List<AnimationPhase> phases, Consumer<AnimationContext> onComplete, Consumer<AnimationContext> onCancel) implements Animation {
+
+    @Override
+    public void play(Player player) {
+
+    }
+}
