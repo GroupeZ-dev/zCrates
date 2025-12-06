f71eec69979e6b850964b9e7db33145f92340678
Traqueur_ <traqueur.dev@gmail.com>
2025-12-06 18:51:39 +0100

    feat: add zItems hook for exemples

---
+++ hooks/zItems/build.gradle.kts
@@ -0,0 +1,8 @@
+repositories {
+    maven(url = "https://repo.groupez.dev/releases")
+    maven(url = "https://repo.groupez.dev/snapshots")
+}
+
+dependencies {
+    compileOnly("fr.traqueur:zitems-api:1b9d3e0")
+}
